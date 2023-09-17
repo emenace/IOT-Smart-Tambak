b9e2b24edaf9ae914c3d561ace46085fb947cd35
Ridhotegar12 <ridhoskuter45@gmail.com>
2023-09-17 23:24:51 +0700

    add new route,chart,css support mobile

--- front-end/Script.js
+++ front-end/Script.js
@@ -24,7 +24,7 @@ function fetchDataAndDisplay() {
 }
 
 // Fungsi ini akan dijalankan setiap satu detik
-setInterval(fetchDataAndDisplay, 59000);
+setInterval(fetchDataAndDisplay, 600000);
 
 // Panggil fungsi ini saat halaman pertama kali dimuat untuk menampilkan data awal
 fetchDataAndDisplay();
@@ -60,105 +60,173 @@ function fetchData() {
         );
 }
 // Panggil fetchData untuk pertama kali
-setInterval(fetchData, 59000);
+setInterval(fetchData, 600000);
 fetchData();
 
+// LINE Chart Suhu AIr 
+fetch('http://localhost:3000/dipasena/chart/suhuAir')
+.then(response => response.json())
+.then(data => {
+    const result = data.result;
+    const latestData = result.slice(Math.max(result.length - 60, 0)); // Ambil 20 data terbaru
 
-// CHART SUHU PERMUKAAN 
-const url = 'http://localhost:3000/dipasena/chart/suhu_per';
-const chart = new Chart(document.getElementById('myChart'), {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            label: 'Suhu Air Permukaan',
-            data: [],
-            backgroundColor: 'rgba(0, 119, 204, 0.3)',
-            borderColor: 'rgba(0, 119, 204, 0.8)',
-            borderWidth: 2,
-            pointRadius: 0
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-            xAxes: [{
-                type: 'time',
-                time: {
-                    unit: 'second'
-                }
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+    const labels = latestData.map(item => item.time1);
+    const suhuAirPermukaan = latestData.map(item => item.suhu_air_permukaan);
+    const suhuAirDasar = latestData.map(item => item.suhu_air_dasar);
+
+    const ctx = document.getElementById('myChart').getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Suhu Air Permukaan',
+                data: suhuAirPermukaan,
+                borderColor: '#35A29F',
+                tension: 0.1
+            },
+            {
+                label: 'Suhu Air Dasar',
+                data: suhuAirDasar,
+                borderColor: ' #071952',
+                tension: 0.1
             }]
         }
-    }
-});
+    });
+})
 
-function updateChart() {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const time = new Date().toLocaleTimeString();
-            const suhu_air_permukaan = data.result[data.count - 1].suhu_air_permukaan;
-            chart.data.labels.push(time);
-            chart.data.datasets[0].data.push(suhu_air_permukaan);
-            chart.update();
-        });
-}
+.catch(error => console.error('Error:', error));
 
-setInterval(updateChart, 1000);
+// Line Chart PH 
+fetch('http://localhost:3000/dipasena/chart/ph')
+.then(response => response.json())
+.then(data => {
+    const result = data.result;
+    const latestData = result.slice(Math.max(result.length - 60, 0)); // Ambil 20 data terbaru
 
-// CHART PH 
-const urlPh = 'http://localhost:3000/dipasena/chart/ph';
-const chartPh = new Chart(document.getElementById('myChartPh'), {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            label: 'pH',
-            data: [],
-            backgroundColor: 'rgba(0, 119, 204, 0.3)',
-            borderColor: 'rgba(0, 119, 204, 0.8)',
-            borderWidth: 2,
-            pointRadius: 0
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-            xAxes: [{
-                type: 'time',
-                time: {
-                    unit: 'second'
-                }
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+    const labels = latestData.map(item => item.time2);
+    const ph = latestData.map(item => item.ph);
+
+    const grafPh = document.getElementById('ChartPh').getContext('2d');
+    new Chart(grafPh, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'pH Air ',
+                data: ph,
+                borderColor: '#1A5D1A',
+                tension: 0.1
             }]
         }
-    }
-});
+    });
+})
+.catch(error => console.error('Error:', error));
 
-function updateChartPh() {
-    fetch(urlPh)
-        .then(response => response.json())
-        .then(data => {
-            const time = new Date().toLocaleTimeString();
-            const ph = data.result[data.count - 1].ph;
-            chartPh.data.labels.push(time);
-            chartPh.data.datasets[0].data.push(ph);
-            chartPh.update();
-        });
-}
+// Line Chart KADAR OKSIGEN
+fetch('http://localhost:3000/dipasena/chart/do')
+.then(response => response.json())
+.then(data => {
+    const result = data.result;
+    const latestData = result.slice(Math.max(result.length - 60, 0)); // Ambil 20 data terbaru
 
-setInterval(updateChartPh, 1000);
+    const labels = latestData.map(item => item.time3);
+    const Oksigen = latestData.map(item => item.oxygen);
+
+    const grafDo = document.getElementById('ChartDo').getContext('2d');
+    new Chart(grafDo, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Kadar Oksigen ',
+                data: Oksigen,
+                borderColor: '#E55604',
+                tension: 0.1
+            }]
+        }
+    });
+})
+.catch(error => console.error('Error:', error));
+
+
+// Line Chart SALINITAS
+fetch('http://localhost:3000/dipasena/chart/salinitas')
+.then(response => response.json())
+.then(data => {
+    const result = data.result;
+    const latestData = result.slice(Math.max(result.length - 60, 0)); // Ambil 20 data terbaru
+
+    const labels = latestData.map(item => item.time4);
+    const salinitas = latestData.map(item => item.salinitas);
+
+    const grafSalinitas = document.getElementById('ChartSalinitas').getContext('2d');
+    new Chart(grafSalinitas, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Salinitas Air ',
+                data: salinitas,
+                borderColor: '#16FF00',
+                tension: 0.1
+            }]
+        }
+    });
+})
+.catch(error => console.error('Error:', error));
+
+// Line Chart Suhu Ruang
+fetch('http://localhost:3000/dipasena/chart/suhuRuang')
+.then(response => response.json())
+.then(data => {
+    const result = data.result;
+    const latestData = result.slice(Math.max(result.length - 60, 0)); // Ambil 20 data terbaru
+
+    const labels = latestData.map(item => item.time5);
+    const suhuRuang = latestData.map(item => item.suhu_ruang);
+
+    const grafSuhuRuang = document.getElementById('ChartSuhuRuang').getContext('2d');
+    new Chart(grafSuhuRuang, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Suhu Ruang ',
+                data: suhuRuang,
+                borderColor: '#FFED00',
+                tension: 0.1
+            }]
+        }
+    });
+})
+.catch(error => console.error('Error:', error));
+
+// Line Chart Amonia
+fetch('http://localhost:3000/dipasena/chart/amonia')
+.then(response => response.json())
+.then(data => {
+    const result = data.result;
+    const latestData = result.slice(Math.max(result.length - 60, 0)); // Ambil 20 data terbaru
+
+    const labels = latestData.map(item => item.time6);
+    const amonia_ = latestData.map(item => item.amonia);
+
+    const grafAmonia = document.getElementById('ChartAmonia').getContext('2d');
+    new Chart(grafAmonia, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Amonia ',
+                data: amonia_,
+                borderColor: '#C21010',
+                tension: 0.1
+            }]
+        }
+    });
+})
+.catch(error => console.error('Error:', error));
 
 
 
@@ -174,3 +242,4 @@ const map = new google.maps.Map(document.getElementById("map"), {
     width: 400,
     height: 300,
 });
+
